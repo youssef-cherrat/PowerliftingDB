53ebbbf96f6a6e475a400ce83105f9639cf725de
Samay Jamakayala <samay.jamakayala@gmail.com>
2024-06-06 22:49:58 -0400

    Initial creation and implementation of Application file to run the intended club application.

---
+++ PowerliftingApplication.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PowerliftingApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("powerlifting_proj.fxml"));
+        Parent root = fxmlLoader.load();
+
+        PowerliftingController controller = fxmlLoader.getController();
+        Configuration configuration = new Configuration();
+//        DatabaseDriver db = new DatabaseDriver(configuration);
+//        PowerliftingService service = new PowerliftingService(db);
+//        service.createTables();
+//        controller.setService(service);
+
+        Scene scene = new Scene(root);
+        stage.setTitle("UVA Club Powerlifting Application");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
